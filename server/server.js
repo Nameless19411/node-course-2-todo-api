25a70e5d0a0d2b091ef385aaef0adb5c5364e13b
OPT ALKO <>
2018-07-26 17:30:18 +0700

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+var _ = require('lodash')
 var express = require('express')
 var bodyParser = require('body-parser')
 var {ObjectID} = require('mongodb')
@@ -46,10 +47,6 @@ app.get('/todos/:id', (req, res) => {
     }).catch((e) => res.status(404).send())
 })
 
-app.listen(port, () => {
-    console.log(`Started on ${port}`)
-})
-
 // DELETE /todos/:id
 app.delete('/todos/:id', (req, res) => {
     var id = req.params.id
@@ -63,6 +60,36 @@ app.delete('/todos/:id', (req, res) => {
         }
         res.send({todo})
     }).catch((e) => res.status(400).send())
+})
+
+app.patch('/todos/:id', (req, res) => {
+    var id = req.params.id
+    var body = _.pick(req.body, ['text', 'completed'])
+
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send()
+    }
+
+    if(_.isBoolean(body.completed) && body.completed){
+        body.completedAt = new Date().getTime()
+    }
+    else{
+        body.completed = false
+        body.completedAt = null
+    }
+
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+        if(!todo){
+            res.status(404).send()
+        }
+        res.send({todo})
+    }).catch((e) => {
+        res.status(400).send()
+    })
+})
+
+app.listen(port, () => {
+    console.log(`Started on ${port}`)
 })
 
 module.exports = {app}
