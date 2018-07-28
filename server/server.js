5cd7a2718a40ae73bb5f9935b1e45c474a22c9cf
OPT ALKO <>
2018-07-28 15:53:39 +0700

    Add POST /users route and token method

--- server/server.js
+++ server/server.js
@@ -14,6 +14,7 @@ var port = process.env.PORT
 
 app.use(bodyParser.json())
 
+// POST /todos
 app.post('/todos', (req, res) => {
     var todo = new Todo({
         text: req.body.text
@@ -26,6 +27,7 @@ app.post('/todos', (req, res) => {
     })
 })
 
+// GET /todos
 app.get('/todos', (req, res) => {
     Todo.find().then((todos) => {
         res.send({todos})
@@ -64,6 +66,7 @@ app.delete('/todos/:id', (req, res) => {
     }).catch((e) => res.status(400).send())
 })
 
+// PATCH /todos/:id
 app.patch('/todos/:id', (req, res) => {
     var id = req.params.id
     var body = _.pick(req.body, ['text', 'completed'])
@@ -87,6 +90,20 @@ app.patch('/todos/:id', (req, res) => {
         res.send({todo})
     }).catch((e) => {
         res.status(400).send()
+    })
+})
+
+// POST /users
+app.post('/users', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password'])
+    var user = new User(body)
+
+    user.save().then(() => {
+        return user.generateAuthToken()
+    }).then((token) => {
+        res.header('x-auth', token).send(user)
+    }).catch((e) => {
+        res.status(400).send(e)
     })
 })
 
